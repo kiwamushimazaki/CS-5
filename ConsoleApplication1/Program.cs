31fd53ed403f484ba3164fcf86fb2c5a327d0ead
kiwamu <kiwamu@KIWAMU-PC>
2016-07-06 11:02:53 +0900

    演習５の解答です。

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication5
 {
-    class Program
+    class Computer
     {
-        static void Main(string[] args)
+        public int memory;
+        public int disk;
+        public string name;
+
+
+
+        public Computer(int computermemory
+                        , string computerName)
         {
+            memory = computermemory;
+            name = computerName;
+        }
+
+        public Computer(int computermemory
+                        , int computerdisk
+                        , string computerName)
+        {
+            memory = computermemory;
+            disk = computerdisk;
+            name = computerName;
+        }
+
+        public void PrintSpec()
+           { 
+             Console.WriteLine("Memory = {0}MB, Disk = {1}GB",memory,disk); 
+           }
+
+    }
+
+
+class MainClass
+    {
+
+        static void Main()
+        {
+            Computer computer1;
+            computer1 = new Computer(1000,"Sales01");
+            Console.WriteLine("{0}", computer1.name);
+            computer1.PrintSpec();
+
+            Computer computer2;
+            computer2 = new Computer(1600, 120, "Sales02");
+            Console.WriteLine("{0}", computer2.name);
+            computer2.PrintSpec();
         }
     }
 }
